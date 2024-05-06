781d388ac222921025bf5c66adffe5f1fe6984ed
Edgardo Alvarez <edgardo@livedesign.org>
2024-05-06 10:21:47 -0500

    primer commit

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './config/envs';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Main');
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  await app.listen(envs.port);
+  logger.log(`App running in port: ${envs.port}`);
+}
+bootstrap();
